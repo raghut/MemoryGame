3456e6813cf734227ce0cbc010a1825601304fde
Raghu <raghu.teegela@gmail.com>
2019-03-09 17:17:54 +0530

    Refactor code

--- app/src/main/java/com/rgu/memorygame/view/activity/HomeActivity.java
+++ app/src/main/java/com/rgu/memorygame/view/activity/HomeActivity.java
@@ -3,6 +3,7 @@ package com.rgu.memorygame.view.activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.rgu.memorygame.enums.GameType;
 import com.rgu.memorygame.R;
@@ -26,14 +27,17 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void startGame(@GameType int level) {
+        activityStartBinding.fragmentContainer.setVisibility(View.VISIBLE);
         gameFragment = GameFragment.newInstance(level);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, gameFragment).commit();
     }
 
     @Override
     public void onBackPressed() {
-        if (getSupportFragmentManager().findFragmentById(R.id.fragment_container) != null) {
+        //Need to optimise
+        if (activityStartBinding.fragmentContainer.getVisibility() == View.VISIBLE) {
             getSupportFragmentManager().popBackStack();
+            activityStartBinding.fragmentContainer.setVisibility(View.GONE);
         } else {
             super.onBackPressed();
         }
